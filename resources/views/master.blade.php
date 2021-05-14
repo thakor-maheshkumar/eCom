94be42bc8073fdf1a984ccb84483d6e39c7ba397
Fv Thakor <fvthakordeveloper@gmail.com>
2021-05-14 14:30:46 +0530

    header, footer and login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>E Comm Title</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+{{ View::make('header') }}
+@yield('content')
+</body>
+<style type="text/css">
+	.custom-login{
+		height: 500px;
+		padding-top: 100px;
+	}
+</style>
+<script type="text/javascript">
+</script>
+</html>
